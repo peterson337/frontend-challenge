271dba45e6133ee0470bba53053955ff75623c0b
Peterson Rodrigues dos Santos <petersonpepi16@gmail.com>
2026-04-24 16:26:48 -0300

    Inicializa o projeto e define a estrutura inicial

---
+++ app/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="bg-white shadow">
+      <p>Header</p>
+    </header>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Bem-vindo</h1>
+    </div>
+  );
+}
