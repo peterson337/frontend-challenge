6956745a8147547699feb0df0a5913440fc8ceeb
Peterson Rodrigues dos Santos <petersonpepi16@gmail.com>
2026-04-30 10:21:17 -0300

    refactor: remove import desnecessário do componente Modal

--- app/components/Ui/Modal.tsx
+++ app/components/Ui/Modal.tsx
@@ -1,166 +1,167 @@
 "use client";
 
-import React, { useState } from "react";
 import Button from "@/app/components/Ui/Button";
-import Share from "@/app/components/icons/Share";
 
-export default function Modal({setIsOpenModal}: {setIsOpenModal: (value: boolean) => void}) {
-
+export default function Modal({
+  setIsOpenModal,
+}: {
+  setIsOpenModal: (value: boolean) => void;
+}) {
   return (
     <>
+      <div
+        className="fixed inset-0 z-50 flex items-center justify-center bg-black/50"
+        onClick={() => setIsOpenModal(false)}
+      >
         <div
-          className="fixed inset-0 z-50 flex items-center justify-center bg-black/50"
-          onClick={() => setIsOpenModal(false)}
+          className="flex flex-col items-stretch rounded-[4px] w-[343px] md:w-[405px] bg-[#FEFEFE]"
+          onClick={(e) => e.stopPropagation()}
         >
-          <div
-            className="flex flex-col items-stretch rounded-[4px] w-[343px] md:w-[405px] bg-[#FEFEFE]"
-            onClick={(e) => e.stopPropagation()}
-          >
-            <div className="flex flex-row justify-between items-center pt-2 pr-2 pb-2 pl-4 border-b border-[#F3F4F6]">
-              <span className="font-inter font-medium text-base leading-[125%] text-[#262626]">
-                Compartilhar curso
+          <div className="flex flex-row justify-between items-center pt-2 pr-2 pb-2 pl-4 border-b border-[#F3F4F6]">
+            <span className="font-inter font-medium text-base leading-[125%] text-[#262626]">
+              Compartilhar curso
+            </span>
+
+            <Button
+              colorButton={{
+                backgroundColor: "bg-white",
+                textColor: "text-black",
+                hover: false,
+              }}
+              rounded="rounded-none"
+              size="w-[38px] h-[38px]"
+              border="border-none"
+              onClick={() => setIsOpenModal(false)}
+            >
+              <span className="flex items-center justify-center">
+                <svg
+                  width="32"
+                  height="32"
+                  viewBox="0 0 32 32"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M12.2 19.8L19.8 12.2"
+                    stroke="#292D32"
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M19.8 19.8L12.2 12.2"
+                    stroke="#292D32"
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
               </span>
+            </Button>
+          </div>
 
-              <Button
-                colorButton={{
-                  backgroundColor: "bg-white",
-                  textColor: "text-black",
-                  hover: false,
-                }}
-                rounded="rounded-none"
-                size="w-[38px] h-[38px]"
-                border="border-none"
-                onClick={() => setIsOpenModal(false)}
-              >
-                <span className="flex items-center justify-center">
-                  <svg
-                    width="32"
-                    height="32"
-                    viewBox="0 0 32 32"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M12.2 19.8L19.8 12.2"
-                      stroke="#292D32"
-                      strokeWidth="1.5"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                    <path
-                      d="M19.8 19.8L12.2 12.2"
-                      stroke="#292D32"
-                      strokeWidth="1.5"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                  </svg>
+          <div className="flex flex-col items-stretch gap-4 p-4">
+            <div className="flex flex-col gap-2">
+              <div className="flex flex-row items-center gap-2 px-3 py-2 bg-[#FEFEFE] border border-[#D1D5DB] shadow-sm">
+                <span className="flex-1 font-inter font-normal text-sm leading-[125%] text-[#404040]">
+                  https//:www.curso.com/iwejfnwieuf...
                 </span>
-              </Button>
-            </div>
 
-            <div className="flex flex-col items-stretch gap-4 p-4">
-              <div className="flex flex-col gap-2">
-                <div className="flex flex-row items-center gap-2 px-3 py-2 bg-[#FEFEFE] border border-[#D1D5DB] shadow-sm">
-                  <span className="flex-1 font-inter font-normal text-sm leading-[125%] text-[#404040]">
-                    https//:www.curso.com/iwejfnwieuf...
+                <Button
+                  colorButton={{
+                    backgroundColor: "bg-white",
+                    textColor: "text-black",
+                    hover: false,
+                  }}
+                  rounded="rounded-none"
+                  border="border-none"
+                  size="p-0"
+                >
+                  <span className="flex items-center justify-center">
+                    <svg
+                      width="16"
+                      height="16"
+                      viewBox="0 0 16 16"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M10.6667 8.6V11.4C10.6667 13.7333 9.73333 14.6667 7.4 14.6667H4.6C2.26667 14.6667 1.33333 13.7333 1.33333 11.4V8.6C1.33333 6.26667 2.26667 5.33333 4.6 5.33333H7.4C9.73333 5.33333 10.6667 6.26667 10.6667 8.6Z"
+                        stroke="#323334"
+                        strokeWidth="1.2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                      <path
+                        d="M14.6667 4.6V7.4C14.6667 9.73333 13.7333 10.6667 11.4 10.6667H10.6667V8.6C10.6667 6.26667 9.73333 5.33333 7.4 5.33333H5.33333V4.6C5.33333 2.26667 6.26667 1.33333 8.6 1.33333H11.4C13.7333 1.33333 14.6667 2.26667 14.6667 4.6Z"
+                        stroke="#323334"
+                        strokeWidth="1.2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
                   </span>
-
-                  <Button
-                    colorButton={{
-                      backgroundColor: "bg-white",
-                      textColor: "text-black",
-                      hover: false,
-                    }}
-                    rounded="rounded-none"
-                    border="border-none"
-                    size="p-0"
-                  >
-                    <span className="flex items-center justify-center">
-                      <svg
-                        width="16"
-                        height="16"
-                        viewBox="0 0 16 16"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="M10.6667 8.6V11.4C10.6667 13.7333 9.73333 14.6667 7.4 14.6667H4.6C2.26667 14.6667 1.33333 13.7333 1.33333 11.4V8.6C1.33333 6.26667 2.26667 5.33333 4.6 5.33333H7.4C9.73333 5.33333 10.6667 6.26667 10.6667 8.6Z"
-                          stroke="#323334"
-                          strokeWidth="1.2"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                        />
-                        <path
-                          d="M14.6667 4.6V7.4C14.6667 9.73333 13.7333 10.6667 11.4 10.6667H10.6667V8.6C10.6667 6.26667 9.73333 5.33333 7.4 5.33333H5.33333V4.6C5.33333 2.26667 6.26667 1.33333 8.6 1.33333H11.4C13.7333 1.33333 14.6667 2.26667 14.6667 4.6Z"
-                          stroke="#323334"
-                          strokeWidth="1.2"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                        />
-                      </svg>
-                    </span>
-                  </Button>
-                </div>
+                </Button>
               </div>
-
-              <Button
-                colorButton={{
-                  backgroundColor: "bg-green",
-                  textColor: "text-white",
-                  hover: false,
-                }}
-                rounded="rounded-none"
-                size="px-3 py-3"
-                border="border-none"
-                width="w-full"
-              >
-                <span className="flex flex-row justify-center items-center gap-[6px]">
-                  <svg
-                    width="16"
-                    height="16"
-                    viewBox="0 0 16 16"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M13.6357 2.32349C12.1312 0.82605 10.1297 0.000854494 7.99963 0C5.8743 0 3.86969 0.824463 2.35529 2.32153C0.838208 3.82117 0.001955 5.81409 0 7.9259V7.92834V7.92981C0.000244376 9.20801 0.336383 10.4974 0.974447 11.6744L0.0218716 16L4.40145 15.0048C5.51067 15.5632 6.74831 15.8577 7.99658 15.8582H7.99976C10.1247 15.8582 12.1293 15.0336 13.644 13.5364C15.1624 12.0355 15.999 10.0452 16 7.93201C16.0006 5.83374 15.1611 3.84192 13.6357 2.32349ZM7.99963 14.6094H7.99682C6.87599 14.6089 5.76543 14.3278 4.78536 13.7961L4.57825 13.6838L1.66603 14.3456L2.2986 11.4735L2.17665 11.2634C1.57036 10.2189 1.24998 9.0658 1.24998 7.92847C1.2523 4.24719 4.27987 1.24878 7.99939 1.24878C9.79628 1.24951 11.4847 1.94543 12.7535 3.20813C14.0415 4.49036 14.7505 6.16785 14.7499 7.93164C14.7484 11.6138 11.7203 14.6094 7.99963 14.6094Z"
-                      fill="#FFFFFF"
-                    />
-                    <path
-                      d="M5.59096 4H5.21376C5.08246 4 4.86928 4.04653 4.68896 4.23227C4.5085 4.41814 4 4.86739 4 5.78106C4 6.69473 4.70539 7.57754 4.8037 7.70158C4.90214 7.82549 6.16531 9.76032 8.16594 10.5047C9.82867 11.1232 10.1671 11.0002 10.5279 10.9692C10.8888 10.9384 11.6925 10.5201 11.8565 10.0865C12.0205 9.65295 12.0205 9.28121 11.9714 9.20358C11.9221 9.1262 11.7908 9.07979 11.594 8.98698C11.3972 8.89405 10.4325 8.43721 10.252 8.37513C10.0715 8.3133 9.94038 8.28232 9.80908 8.46832C9.67778 8.65394 9.29125 9.0834 9.17638 9.20731C9.06164 9.33135 8.94677 9.3469 8.74989 9.25397C8.553 9.16078 7.92556 8.96123 7.17364 8.32873C6.58839 7.83644 6.18227 7.20866 6.0674 7.02279C5.95266 6.83705 6.05517 6.73653 6.15388 6.64384C6.24233 6.56074 6.3618 6.44702 6.46024 6.33866C6.55855 6.23018 6.58655 6.15279 6.65226 6.02888C6.71784 5.90497 6.68499 5.79648 6.63583 5.70367C6.58655 5.61074 6.20882 4.69247 6.03375 4.32521H6.03388C5.88642 4.01592 5.7312 4.00547 5.59096 4Z"
-                      fill="#FFFFFF"
-                    />
-                  </svg>
-
-                  <span className="font-inter font-medium text-xs leading-[125%] text-[#FAFAFA]">
-                    Compartilhar via Whatsapp
-                  </span>
-                </span>
-              </Button>
             </div>
 
-            <div className="flex flex-row justify-end items-center gap-2 px-4 py-3 border-t border-[#E5E7EB]">
-              <Button
-                colorButton={{
-                  backgroundColor: "bg-white",
-                  textColor: "text-black",
-                  hover: false,
-                }}
-                rounded="rounded-none"
-                size="px-3 py-0"
-                width="w-[76px]"
-                heigth="h-[36px]"
-                border="border border-[#E5E7EB]"
-                onClick={() => setIsOpenModal(false)}
-              >
-                <span className="font-medium text-xs leading-[125%] text-[#404040]">
-                  Fechar
+            <Button
+              colorButton={{
+                backgroundColor: "bg-green",
+                textColor: "text-white",
+                hover: false,
+              }}
+              rounded="rounded-none"
+              size="px-3 py-3"
+              border="border-none"
+              width="w-full"
+            >
+              <span className="flex flex-row justify-center items-center gap-[6px]">
+                <svg
+                  width="16"
+                  height="16"
+                  viewBox="0 0 16 16"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M13.6357 2.32349C12.1312 0.82605 10.1297 0.000854494 7.99963 0C5.8743 0 3.86969 0.824463 2.35529 2.32153C0.838208 3.82117 0.001955 5.81409 0 7.9259V7.92834V7.92981C0.000244376 9.20801 0.336383 10.4974 0.974447 11.6744L0.0218716 16L4.40145 15.0048C5.51067 15.5632 6.74831 15.8577 7.99658 15.8582H7.99976C10.1247 15.8582 12.1293 15.0336 13.644 13.5364C15.1624 12.0355 15.999 10.0452 16 7.93201C16.0006 5.83374 15.1611 3.84192 13.6357 2.32349ZM7.99963 14.6094H7.99682C6.87599 14.6089 5.76543 14.3278 4.78536 13.7961L4.57825 13.6838L1.66603 14.3456L2.2986 11.4735L2.17665 11.2634C1.57036 10.2189 1.24998 9.0658 1.24998 7.92847C1.2523 4.24719 4.27987 1.24878 7.99939 1.24878C9.79628 1.24951 11.4847 1.94543 12.7535 3.20813C14.0415 4.49036 14.7505 6.16785 14.7499 7.93164C14.7484 11.6138 11.7203 14.6094 7.99963 14.6094Z"
+                    fill="#FFFFFF"
+                  />
+                  <path
+                    d="M5.59096 4H5.21376C5.08246 4 4.86928 4.04653 4.68896 4.23227C4.5085 4.41814 4 4.86739 4 5.78106C4 6.69473 4.70539 7.57754 4.8037 7.70158C4.90214 7.82549 6.16531 9.76032 8.16594 10.5047C9.82867 11.1232 10.1671 11.0002 10.5279 10.9692C10.8888 10.9384 11.6925 10.5201 11.8565 10.0865C12.0205 9.65295 12.0205 9.28121 11.9714 9.20358C11.9221 9.1262 11.7908 9.07979 11.594 8.98698C11.3972 8.89405 10.4325 8.43721 10.252 8.37513C10.0715 8.3133 9.94038 8.28232 9.80908 8.46832C9.67778 8.65394 9.29125 9.0834 9.17638 9.20731C9.06164 9.33135 8.94677 9.3469 8.74989 9.25397C8.553 9.16078 7.92556 8.96123 7.17364 8.32873C6.58839 7.83644 6.18227 7.20866 6.0674 7.02279C5.95266 6.83705 6.05517 6.73653 6.15388 6.64384C6.24233 6.56074 6.3618 6.44702 6.46024 6.33866C6.55855 6.23018 6.58655 6.15279 6.65226 6.02888C6.71784 5.90497 6.68499 5.79648 6.63583 5.70367C6.58655 5.61074 6.20882 4.69247 6.03375 4.32521H6.03388C5.88642 4.01592 5.7312 4.00547 5.59096 4Z"
+                    fill="#FFFFFF"
+                  />
+                </svg>
+
+                <span className="font-inter font-medium text-xs leading-[125%] text-[#FAFAFA]">
+                  Compartilhar via Whatsapp
                 </span>
-              </Button>
-            </div>
+              </span>
+            </Button>
+          </div>
+
+          <div className="flex flex-row justify-end items-center gap-2 px-4 py-3 border-t border-[#E5E7EB]">
+            <Button
+              colorButton={{
+                backgroundColor: "bg-white",
+                textColor: "text-black",
+                hover: false,
+              }}
+              rounded="rounded-none"
+              size="px-3 py-0"
+              width="w-[76px]"
+              heigth="h-[36px]"
+              border="border border-[#E5E7EB]"
+              onClick={() => setIsOpenModal(false)}
+            >
+              <span className="font-medium text-xs leading-[125%] text-[#404040]">
+                Fechar
+              </span>
+            </Button>
           </div>
         </div>
+      </div>
     </>
   );
 }
