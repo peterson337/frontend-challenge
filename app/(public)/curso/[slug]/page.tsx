779f118238f3a43855662eb2be316849344327f8
Peterson Rodrigues dos Santos <petersonpepi16@gmail.com>
2026-05-01 20:00:23 -0300

    fix: Corrige lógica de extração de dados da API

--- app/(public)/curso/[slug]/page.tsx
+++ app/(public)/curso/[slug]/page.tsx
@@ -1,6 +1,8 @@
 import { use } from "react";
 import useFetchData from "@/app/hook/useFetchData";
 import CourseDetailActions from "@/app/(public)/curso/[slug]/CourseDetailActions";
+import { Course } from "@/app/types/courses";
+
 type CourseDetailPageProps = {
   params: Promise<{ slug: string }>;
 };
@@ -8,12 +10,21 @@ type CourseDetailPageProps = {
 export default function CourseDetailPage({ params }: CourseDetailPageProps) {
   // Função utilitária para realizar requisições à API, recebendo o endpoint como parâmetro.
   const { getCourses } = useFetchData();
-  const { slug } = use(params);
+  const { slug: urlAmigavel } = use(params);
 
-  const courseData = use(getCourses(`courses/${slug}`));
+  const fetchData = use(getCourses(`courses/${urlAmigavel}`));
 
   // Extrai apenas os dados necessários da resposta da API para serem utilizados na aplicação
-  const { title, banner, long_description, id } = courseData;
+  const { title, banner, long_description, id, slug, teachers } = fetchData;
+
+  const courseData = {
+    id,
+    title,
+    banner,
+    slug,
+    teachers,
+    long_description,
+  };
 
   return (
     <>
